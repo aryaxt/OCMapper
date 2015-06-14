1a01fa6d4ce7a61022cf97e7f12a9f02280f09d6
Aryan <aryaxt@gmail.com>
2015-06-14 13:21:19 -0700

    Use NS_ENUM instead of enum

--- OCMapper/Source/Instance Provider/ManagedObjectInstanceProvider.h
+++ OCMapper/Source/Instance Provider/ManagedObjectInstanceProvider.h
@@ -29,10 +29,10 @@
 #import <CoreData/CoreData.h>
 #import "InstanceProvider.h"
 
-typedef enum {
+typedef NS_ENUM(NSUInteger, UpsertMode) {
 	UpsertModeUpdateExistingObject,
 	UpsertModePurgeExistingObject
-}UpsertMode;
+};
 
 @interface ManagedObjectInstanceProvider : NSObject <InstanceProvider>
 
